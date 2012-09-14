73f4f66564a4ce02b0bd8d7550177eede13f312b
Demian Brecht <demianbrecht@gmail.com>
2012-09-14 07:14:00 -0700

    added changes.txt

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, "README.rst")).read()
+README = README + open(os.path.join(here, "CHANGES.txt")).read()
 
 requires = []
 
