0c4e3cdfe5e3ea8beab01c2f7ff1091021aa1930
Witold Baryluk <witold.baryluk+github@gmail.com>
2019-12-11 16:25:22 +0000

    Simplify code
    Simplify the main code. It is still functionally identical, just with a bit less tokens and special cases.

--- sanction/__init__.py
+++ sanction/__init__.py
@@ -1,7 +1,7 @@
 # vim: set ts=4 sw=)
 
 from functools import wraps
-from json import loads
+import json
 from datetime import datetime, timedelta
 from time import mktime
 try:
@@ -9,32 +9,40 @@ try:
     from urllib2 import Request, urlopen
     from urlparse import urlsplit, urlunsplit, parse_qsl
 
-    # monkeypatch httpmessage
     from httplib import HTTPMessage
     def get_charset(self):
         try:
-            data = filter(lambda s: 'Content-Type' in s, self.headers)[0]
+            data = self.headers.getheader('Content-Type')
             if 'charset' in data:
-                cs = data[data.index(';') + 1:-2].split('=')[1].lower()
-                return cs
+                return data.split(';')[1].split('=')[1].lower()
         except IndexError:
-            pass
-
-        return 'utf-8'
+            return 'utf-8'
+    # monkeypatch HTTPMmessage
     HTTPMessage.get_content_charset = get_charset 
 except ImportError: # pragma: no cover
     from urllib.parse import urlencode, urlsplit, urlunsplit, parse_qsl
     from urllib.request import Request, urlopen
 
+    
+def _request(url, data, method):
+    # This is not even really needed. It is in case somebody wants to use
+    # method other than GET / POST, which is a bit of a stretch.
+    try:
+        # Python 3.3+ (2012).
+        return Request(url, data=data, method=method)
+    else:
+        req = Request(url, data=data)
+        req.get_method = lambda: method
+        return req
+
 
 class Client(object):
-    """ OAuth 2.0 client object
-    """
+    """OAuth 2.0 client object"""
 
     def __init__(self, auth_endpoint=None, token_endpoint=None,
         resource_endpoint=None, client_id=None, client_secret=None,
         token_transport=None):
-        """ Instantiates a `Client` to authorize and authenticate a user
+        """Instantiates a `Client` to authorize and authenticate a user
 
         :param auth_endpoint: The authorization endpoint as issued by the
                               provider. This is where the user should be
@@ -47,6 +55,7 @@ class Client(object):
         :param client_id: The client ID as issued by the provider.
         :param client_secret: The client secret as issued by the provider. This
                               must not be shared.
+        :param token_transport: ???
         """
         assert token_transport is None or hasattr(token_transport, '__call__')
 
@@ -60,10 +69,9 @@ class Client(object):
         self.token_expires = -1
         self.refresh_token = None
 
-    def auth_uri(self, redirect_uri=None, scope=None, scope_delim=None, 
-        state=None, **kwargs):
+    def auth_uri(self, redirect_uri=None, scope=None, state=None, **kwargs):
 
-        """  Builds the auth URI for the authorization endpoint
+        """Builds the auth URI for the authorization endpoint
 
         :param scope: (optional) The `scope` parameter to pass for
                       authorization. The format should match that expected by
@@ -81,13 +89,13 @@ class Client(object):
             'response_type': 'code',
         })
 
-        if scope is not None:
+        if scope:
             kwargs['scope'] = scope
 
-        if state is not None:
+        if state:
             kwargs['state'] = state
 
-        if redirect_uri is not None:
+        if redirect_uri:
             kwargs['redirect_uri'] = redirect_uri
 
         return '%s?%s' % (self.auth_endpoint, urlencode(kwargs))
@@ -114,7 +122,8 @@ class Client(object):
         :param parser: Callback to deal with returned data. Not all providers
                        use JSON.
         """
-        kwargs = kwargs and kwargs or {}
+        if not kwargs:
+            kwargs = {}
 
         parser = parser or _default_parser
         kwargs.update({
@@ -123,14 +132,13 @@ class Client(object):
             'grant_type': 'grant_type' in kwargs and kwargs['grant_type'] or \
                 'authorization_code'
         })
-        if redirect_uri is not None:
-            kwargs.update({'redirect_uri': redirect_uri})
+        if redirect_uri:
+            kwargs['redirect_uri'] = redirect_uri
 
         # TODO: maybe raise an exception here if status code isn't 200?
         msg = urlopen(self.token_endpoint, urlencode(kwargs).encode(
             'utf-8'))
-        data = parser(msg.read().decode(msg.info().get_content_charset() or
-            'utf-8'))
+        data = parser(msg.read().decode(msg.info().get_content_charset()))
 
         for key in data:
             setattr(self, key, data[key])
@@ -159,23 +167,19 @@ class Client(object):
         :param parser: Parser callback to deal with the returned data. Defaults
                        to ``json.loads`.`
         """
-        assert self.access_token is not None
-        parser = parser or loads 
+        assert self.access_token
+        parser = parser or _default_parser 
 
-        if not method:
-            method = 'GET' if not data else 'POST'
-
-        req = self.token_transport('{0}{1}'.format(self.resource_endpoint, 
-            url), self.access_token, data=data, method=method, headers=headers)
+        transport_uri = '{0}{1}'.format(self.resource_endpoint, url)
+        req = self.token_transport(transport_uri, self.access_token,
+                                   data=data, method=method, headers=headers)
 
         resp = urlopen(req)
         data = resp.read()
         try:
-            return parser(data.decode(resp.info().get_content_charset() or
-                'utf-8'))
+            return parser(data.decode(resp.info().get_content_charset(failobj="utf-8"))))
             # try to decode it first using either the content charset, falling
             # back to utf-8
-
         except UnicodeDecodeError:
             # if we've gotten a decoder error, the calling code better know how
             # to deal with it. some providers (i.e. stackexchange) like to gzip
@@ -184,43 +188,30 @@ class Client(object):
             return parser(data)
 
 
-def transport_headers(url, access_token, data=None, method=None, headers=None):
-    try:
-        req = Request(url, data=data, method=method)
-    except TypeError:
-        req = Request(url, data=data)
-        req.get_method = lambda: method
-
+def transport_headers(url, access_token, data=None, method=None, headers={}):
+    req = _request(url, data=data, method=method)
+    
     add_headers = {'Authorization': 'Bearer {0}'.format(access_token)}
-    if headers is not None:
-        add_headers.update(headers)
+    add_headers.update(headers)
 
     req.headers.update(add_headers)
     return req
 
 
-def transport_query(url, access_token, data=None, method=None, headers=None):
+def transport_query(url, access_token, data=None, method=None, headers={}):
+    req = _request(url, data=data, method=method)
+
     parts = urlsplit(url)
     query = dict(parse_qsl(parts.query))
-    query.update({
-        'access_token': access_token
-    })
+    query['access_token'] = access_token
     url = urlunsplit((parts.scheme, parts.netloc, parts.path,
         urlencode(query), parts.fragment))
-    try:
-        req = Request(url, data=data, method=method)
-    except TypeError:
-        req = Request(url, data=data)
-        req.get_method = lambda: method
-
-    if headers is not None:
-        req.headers.update(headers)
-
+    req.headers.update(headers)
     return req
 
 
 def _default_parser(data):
     try:
-        return loads(data)
+        return json.loads(data)
     except ValueError:
         return dict(parse_qsl(data))
